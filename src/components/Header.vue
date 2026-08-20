3b60ea59346e96af7a22c48799216ea2ee706438
agent1156 <agent1156@yandex.ru>
2026-08-20 13:17:10 +0300

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,62 @@
+<template>
+    <header>
+        <div class="head_top">
+            <div class="head_top_cont">
+                <a href=""></a>
+                <div class="head_top_cont_menu">
+                    <button>Каталог</button>
+                </div>
+                <div class="head_top_cont_search">
+                    <form action="">
+                        <div class="head_top_cont_search_block">
+                            <div class="head_top_cont_search_block_lisk">
+                                везде
+                            </div>
+                            <div class="head_top_cont_search_block_inpup">
+                                <input type="text" name="" id="">
+                                <div class='head_top_cont_search_block_img'>
+                                    <img src="" alt="">
+                                </div>
+                            </div>
+                            <input type="submit" class="head_top_cont_search_block_submit">
+                        </div>
+                    </form>
+                </div>
+                <div class="head_top_cont_blok_login">
+                    <div class="head_top_cont_blok_login_item">
+                        <div class="head_top_cont_blok_login_item_img">
+                            <img src="" alt="">
+                        </div>
+                        <span>Войти</span>
+                    </div>
+                    <div class="head_top_cont_blok_login_item">
+                        <div class="head_top_cont_blok_login_item_img">
+                            <img src="" alt="">
+                        </div>
+                        <span>Войти</span>
+                    </div>
+                    <div class="head_top_cont_blok_login_item">
+                        <div class="head_top_cont_blok_login_item_img">
+                            <img src="" alt="">
+                        </div>
+                        <span>Войти</span>
+                    </div>
+                    <div class="head_top_cont_blok_login_item">
+                        <div class="head_top_cont_blok_login_item_img">
+                            <img src="" alt="">
+                        </div>
+                        <span>Войти</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="head_below">
+
+        </div>
+    </header>
+</template>
+<script>
+</script>
+<style>
+
+</style>
