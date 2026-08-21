7f93dd36dccb8cdd146942a4a75a6a29700495bb
agent1156 <agent1156@yandex.ru>
2026-08-21 10:46:03 +0300

    Доработал шапку

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,50 +2,55 @@
     <header>
         <div class="head_top">
             <div class="head_top_cont">
-                <a href=""></a>
+                <div class="head_top_cont_logo">
+                    <div class="head_top_cont_img">
+                    <a href="">
+                        <img src="https://ir.ozone.ru/s3/cms/d2/t26/wc400/logo_ozon_1.png" alt="">
+                    </a>
+                </div>
                 <div class="head_top_cont_menu">
                     <button>Каталог</button>
+                </div>
                 </div>
                 <div class="head_top_cont_search">
                     <form action="">
                         <div class="head_top_cont_search_block">
-                            <div class="head_top_cont_search_block_lisk">
-                                везде
-                            </div>
-                            <div class="head_top_cont_search_block_inpup">
-                                <input type="text" name="" id="">
+                            
+                            <div class="head_top_cont_search_block_cont">
+                                <input type="text" name="" id="" class="head_top_cont_search_block_inpup">
                                 <div class='head_top_cont_search_block_img'>
                                     <img src="" alt="">
                                 </div>
+                                 <button class="head_top_cont_search_block_submit"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" class="ag5_12_2-b0"><path fill="currentColor" d="M17.892 15.064a8 8 0 1 0-2.828 2.828l2.522 2.522a2 2 0 1 0 2.828-2.828zM11 16a5 5 0 1 1 0-10 5 5 0 0 1 0 10"></path></svg></button>
                             </div>
-                            <input type="submit" class="head_top_cont_search_block_submit">
+                           
                         </div>
                     </form>
                 </div>
                 <div class="head_top_cont_blok_login">
                     <div class="head_top_cont_blok_login_item">
                         <div class="head_top_cont_blok_login_item_img">
-                            <img src="" alt="">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 16 16" class="" style="color: var(--graphicPrimary);"><path fill="currentColor" d="M5.161 9.262c.424-.17.888-.043 1.318.11a4.6 4.6 0 0 0 1.515.265 4.6 4.6 0 0 0 1.505-.261c.437-.153.909-.278 1.337-.105.73.297 1.551.899 2.02 1.616a.88.88 0 0 1-.082 1.067C11.65 13.216 9.811 14 7.994 14s-3.643-.782-4.768-2.045a.88.88 0 0 1-.083-1.067c.47-.722 1.285-1.333 2.018-1.626M7.994 2c1.84 0 3.333 1.465 3.333 3.272 0 1.808-1.492 3.274-3.333 3.274-1.84 0-3.332-1.466-3.332-3.274C4.662 3.465 6.154 2 7.994 2"></path></svg>
                         </div>
                         <span>Войти</span>
                     </div>
                     <div class="head_top_cont_blok_login_item">
                         <div class="head_top_cont_blok_login_item_img">
-                            <img src="" alt="">
+                           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="" style="color: var(--graphicTertiary);"><path fill="currentColor" d="M14.692 5.694c.368-.205.365-.469-.009-.664C13.367 4.343 12.708 4 12 4s-1.367.343-2.683 1.03l-2 1.044c-1.614.842-2.42 1.263-2.869 2.02C4 8.85 4 9.79 4 11.673v1.652c0 1.883 0 2.824.448 3.58s1.255 1.178 2.869 2.02l2 1.044C10.633 20.657 11.292 21 12 21s1.367-.343 2.683-1.03l2-1.044c1.614-.842 2.42-1.263 2.869-2.02.448-.756.448-1.697.448-3.58v-1.652c0-1.883 0-2.824-.448-3.58-.329-.556-.851-.93-1.744-1.423-.367-.203-.389-.204-.763.004L11 10c-.344.19-.739.394-.91.77-.09.197-.09.375-.09.73V14a1 1 0 0 1-2 0v-4a1 1 0 0 1 .514-.874z"></path></svg>
                         </div>
-                        <span>Войти</span>
+                        <span>Заказы</span>
                     </div>
                     <div class="head_top_cont_blok_login_item">
                         <div class="head_top_cont_blok_login_item_img">
-                            <img src="" alt="">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="" style="color: var(--graphicTertiary);"><path fill="currentColor" d="M3 10.163C3 7.262 5.13 5 8 5c1.929 0 3.244 1.102 4 2.066C12.756 6.102 14.071 5 16 5c2.87 0 5 2.264 5 5.163 0 4.561-4.568 7.856-8.243 9.66a1.71 1.71 0 0 1-1.514 0C7.568 18.02 3 14.724 3 10.163"></path></svg>
                         </div>
-                        <span>Войти</span>
+                        <span>Избранное</span>
                     </div>
                     <div class="head_top_cont_blok_login_item">
                         <div class="head_top_cont_blok_login_item_img">
-                            <img src="" alt="">
+                           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="" style="color: var(--graphicTertiary);"><path fill="currentColor" d="M9.925 5.371a1 1 0 1 0-1.858-.742L6.317 9h-1.2c-1.076 0-1.614 0-1.913.346-.3.346-.222.878-.067 1.942l.271 1.864c.475 3.265.902 4.898 2.03 5.873s2.778.975 6.08.975h.96c3.302 0 4.953 0 6.08-.975 1.128-.975 1.559-2.608 2.034-5.873l.271-1.864c.155-1.064.233-1.596-.067-1.942S19.96 9 18.883 9h-1.205l-1.75-4.371a1 1 0 0 0-1.857.742L15.523 9h-7.05zM10.997 14v2a1 1 0 0 1-2 0v-2a1 1 0 0 1 2 0M14 13a1 1 0 0 1 1 1v2a1 1 0 0 1-2 0v-2a1 1 0 0 1 1-1"></path></svg>
                         </div>
-                        <span>Войти</span>
+                        <span>Корзина</span>
                     </div>
                 </div>
             </div>
@@ -58,5 +63,123 @@
 <script>
 </script>
 <style>
+/* ===== Базовые стили для всего приложения ===== */
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 
+body {
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  background: #f5f7fa;
+  line-height: 1.6;
+}
+
+
+
+img {
+  max-width: 100%;
+  height: auto;
+  display: block;
+}
+
+button {
+  cursor: pointer;
+  border: none;
+  outline: none;
+  font-family: inherit;
+}
+
+
+
+
+header{
+    max-width: 1416px;
+    background: white;
+    padding: 8px 24px;
+
+}
+.head_top_cont_menu button{
+    background: #005bff;
+    color: #fff;
+    font-size: 16px;
+    font-weight: 600;
+    font-family: font-familyOzon-Sans-VF, arial, sans-serif;
+    width: 126px;
+    height: 44px;
+    border-radius: 8px;
+}
+.head_top_cont{
+    display: flex;
+}
+.head_top_cont_img{
+    width: 140px;
+    height: 44px;
+}
+.head_top_cont_search_block{
+    display: flex;
+}
+.head_top_cont_search_block_inpup{
+    width: 422px;
+    display: block;
+    /* margin: 0px 14px; */
+    padding: 10px;
+    border-radius: 7px;
+    background: rgb(255 255 255);
+    border: 0px solid #005bff;
+    color:black;
+   
+}
+.head_top_cont_search_block_inpup:focus {
+    border: none;
+    outline: none; 
+}
+.head_top_cont_search_block_cont{
+     position: relative;
+     width: 512px;
+    display: block;
+    margin: 0px 14px;
+    
+    border-radius: 10px;
+    background: white;
+    border: 3px solid #005bff;
+}
+.head_top_cont_search_block_submit{
+   background: #005bff;
+    height: 36px;
+    padding: 10px;
+    position: absolute;
+    top: 0px;
+    right: 0px;
+    width: 81px;
+    border-radius: 6px;
+    border: navajowhite;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    }
+    .head_top_cont_blok_login{
+        display: flex;
+        display: flex;
+    justify-content: space-evenly;
+    width: 267px;
+    }
+    .head_top_cont_blok_login span{
+        font-size: 12px;
+    }
+    .head_top_cont_blok_login_item{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 0px;
+    }
+    .head_top_cont_logo{
+        display: flex;
+        gap: 5px;
+    }
+        
 </style>
